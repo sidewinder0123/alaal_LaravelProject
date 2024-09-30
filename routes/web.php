73caa13e04c707f96f8a7411edf251b7aa2b81dd
sidewinder0123 <alaaljamesmatthew@gmai.com>
2024-09-30 18:36:36 +0800

    message

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware(['auth', 'verified'])->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
