7b989fac5d2ce58b3c6861106a48680dcc1ebeac
sidewinder0123 <alaaljamesmatthew@gmail.com>
2024-10-07 18:28:34 +0800

    tite

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+                    {{ __("You're logged in nigga!") }}
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="p-6 text-gray-900 dark:text-gray-100"
+    >
+    <h3 class="bg-blue-400 p-8 rounded-lg text-black"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. A unde maiores in optio exercitationem ducimus praesentium fugit, officiis sit soluta quibusdam saepe dicta minima! Fuga vero eius dolorem deleniti libero.</h3>
+        
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get ('/', function () {
+    return view('welcome');
+});
+
 Route::get('/', function () {
     return view('welcome');
 })->middleware(['auth', 'verified'])->name('welcome');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
